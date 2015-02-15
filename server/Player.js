55a9097b1b5383b55e156fde29e95118e6dcbea3
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-02-15 10:33:49 -0500

    Movement and player handle stub

--- server/Player.js
+++ server/Player.js
@@ -11,10 +11,26 @@ function Player(ws, db) {
   for(var key in Object.keys(db)) {
     this[key] = db[key];
   }
+
+  // init some ram-only fields
+
+  this.x = 0;
+  this.y = 0;
 }
 
 Player.prototype.send = function(msg) {
   this.ws.send(JSON.stringify(msg));
+}
+
+// messages of type player are routed here
+
+Player.prototype.handle = function(msg) {
+  if(msg.subtype == "move") {
+    // TODO: sanity checking
+    // TODO: broadcasting new position
+    this.x = msg.x || 0;
+    this.y = msg.y || 0;
+  }
 }
 
 module.exports = Player;
