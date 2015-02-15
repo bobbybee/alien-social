cf0684793982c23e26b70aae8bcbc7652c536cc6
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-02-14 22:43:25 -0500

    AuthenticationManager class

---
+++ server/AuthenticationManager.js
@@ -0,0 +1,15 @@
+// AuthenticationManager takes a username and password pair
+// and returns either a player object if the auth was successfull
+// or null if it fails
+// TODO: error codes
+
+var Player = require("./Player");
+
+module.exports = function(username, password) {
+  // TODO: hook up DB
+  // for now, we just authenticate all requests blindly
+
+  return new Player({
+    username: username
+  });
+}
