a3d7a20db0cbebab048c1d66de84ddc61accf737
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-02-14 22:57:16 -0500

    Integrate AuthenticationManager

--- server/handleMessage.js
+++ server/handleMessage.js
@@ -1,8 +1,23 @@
+var AuthenticationManager = require("./AuthenticationManager");
+
 module.exports = function(ws, msg) {
   console.log("Received message " + msg);
 
   if(msg.type == "auth") {
-    console.log("Authentication request using credentials " + msg.username + " " + msg.password);
+    if(!ws.player) {
+      ws.player = AuthenticationManager(msg.username, msg.password);
+
+      if(ws.player) {
+        console.log("Login success");
+        // TODO: alert client of success
+      } else {
+        console.log("Login fail");
+        // TODO: alert client of failure
+      }
+    } else {
+      console.log("Already logged in");
+      // TODO: kick client
+    }
   } else {
     console.warn("Unknown message type " + msg.type);
   }
